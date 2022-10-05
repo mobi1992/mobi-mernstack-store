4c2c252418b6dff87b22551484dc571ea0b8c422
mobi1992 <mobeenamushtaq2@gmail.com>
2022-10-05 13:23:02 +0500

    final version

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store'
 import { transitions, positions, Provider as AlertProvider } from 'react-alert'
 import AlertTemplate from 'react-alert-template-basic'
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = document.getElementById('root');
 
 const options = {
   // you can also just use 'bottom center'
@@ -16,11 +16,12 @@ const options = {
   // you can also just use 'scale'
   transition: transitions.SCALE
 }
-root.render(
+render(
   <Provider store={store}>
     <AlertProvider template = {AlertTemplate} {...options }>
       <App />
     </AlertProvider>
-  </Provider>
+  </Provider>,
+  root
 );
 
